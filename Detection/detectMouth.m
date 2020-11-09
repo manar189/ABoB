d1d1ca9a2a5cb11a87fd45a966f390b01008f890
Simon <simbr615@student.liu.se>
2020-11-09 10:25:04 +0100

    Skeleton
    skeleton to be completed

---
+++ Detection/detectMouth.m
@@ -0,0 +1,9 @@
+function [mouthMask, middleOfMouth] = detectMouth(inImage)
+%DETECTMOUTH Summary of this function goes here
+%   Detailed explanation goes here
+mouthMask = inImage;
+middleOfMouth = [300, 300];
+
+
+end
+
