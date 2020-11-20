a33ddf912b49bbd16bb2a0d99cee5c0b281b9a7e
Mans Aronsson <m.k.j.aronsson@gmail.com>
2020-11-20 11:45:34 +0100

    Changed normalization so that both eyes are in the same position in each image

--- Detection/normalizeFace.m
+++ Detection/normalizeFace.m
@@ -1,42 +1,54 @@
-function outimg = normalizeFace(lEye,rEye,image)
+function outIm = normalizeFace(lEye,rEye,im)
 %NORMALIZEFACE Takes the positions of the eyes of a face in an image and
 %the image, and returns a normalized version of the image.
 %   
-%   Normalisere ansiktet till en 271x221 bild genom att först rotera ögeonen
-%   till x-axeln och sedan translateras vänsterögat till bildens center
-%   förskutet med -50 och -25.
+%   Detailed explanation goes here
 
 if (lEye == -1) | (rEye == -1)
     % We could make it so that if we can't find eyes, it normalizes around
     % the centroid of the masked face image.
-    outimg = im2gray(image);
+    outIm = im2gray(im);
     return
 end
 
-image = rgb2gray(image);
-[row , col] = size(image);
+targetSize = [440 300];     % Size of output image
+targetEyeDistance = 150;    % Scales the image so eyes are this far apart
+im = rgb2gray(im2double(im));
+[m,n] = size(im);
 
-blackImage = zeros(row,col);
-blackImage(lEye(2),lEye(1)) = 1;
-%blackImage(round(Reye(2)),round(Reye(1))) = 1;
+eyeDist = pdist([lEye(1) lEye(2); rEye(1) lEye(2)], 'cityblock');
 angle = rad2deg(atan2(rEye(2)-lEye(2),rEye(1)-lEye(1)));
-image = imrotate(image,angle);
-blackIM = imrotate(blackImage,angle);
 
-[leyeRow,leyeCol] = find(blackIM == 1);
-lEye = [leyeCol(1),leyeRow(1)];
+% Rotate around image centrum
+im = imrotate(im,angle,'bilinear','crop');
+% figure, imshow(im);
 
-[row , col,~] = size(image);
-centerRow = floor(row/2); %Y
-centerCol = floor(col/2); %X
-eyePos = [ centerCol-50 centerRow-25];
+% Transformation matrix of the rotation
+T1 = [1 0 (n/2);
+      0 1 (m/2);
+      0 0 1];
+R = [cosd(angle) sind(angle) 0;
+     -sind(angle) cosd(angle) 0;
+     0 0 1];
+T2 = [1 0 -(n/2);
+      0 1 -(m/2);
+      0 0 1];
+M1 = T1*R*T2;
+% Transforms eye coordinates to new position
+lEye = round(M1*[lEye 1]')';
+% rEye = round(M1*[rEye 1]')';
 
+% Translates face to center of image
+im = imtranslate(im, [-lEye(1)+n/2-eyeDist/2 -lEye(2)+m/2-eyeDist/4]);
+% figure, imshow(im);
 
-T = eyePos - lEye;
-lEye = eyePos;
+% Scales image so face is same size on all images
+scale = targetEyeDistance / eyeDist;
+im = imresize(im,scale);
+% figure, imshow(im);
 
-
-outimg = imtranslate(image,T);
-outimg = imcrop(outimg,[ lEye(1)-53 lEye(2)-90 220 270]);
+% Crops image to target size
+crop = centerCropWindow2d(size(im),targetSize);
+outIm = imcrop(im, crop);
 
 end
